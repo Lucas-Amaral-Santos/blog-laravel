87dfec1cbad37267042a8aa1a4f927d84f33950a
unknown <lucas.amaral@afr.org.br>
2023-03-13 10:35:00 -0300

    Search

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,17 +9,21 @@ use App\Models\Category;
 class PostController extends Controller
 {
     public function index() {
-        $posts = Post::latest();
 
-        if (request('search')){
-            //
-            $posts->where('title', 'like', '%' . request('search') . '%')
-                ->orWhere('body', 'like', '%' . request('search') . '%');
-        }
 
-        return view('posts', [
-            'posts' => $posts->get(),
-            'categories' => Category::all()
+        return view('posts.index', [
+            'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->get(),
         ]);
     }
+
+    public function show(Post $post) {
+
+        return view('posts.show', [
+            'post' => $post
+        ]);
+
+    }
+
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,75 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\Post;
-use App\Models\User;
-use App\Models\Category;
 use \App\Http\Controllers\PostController;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
-Route::get('posts/{post:slug}', function(Post $post){
-
-    // Especifica caminho relacionado ao slug
-    // $path = __DIR__."/../resources/posts/{$slug}.html";
-
-    //se slug não existe
-    // if(! file_exists($path)){
-    //   ddd("File does not exists!");
-    // }
-
-    // Carrega conteudo do arquivo
-    //$post = file_get_contents($path);
-
-    // Mesma operação usando cache por 1 hora
-    // $post = cache()->remember("posts.{$slug}", 1200, function() use ($path) {
-    //   var_dump('file_get_contents');
-    //   return file_get_contents($path);
-    // });
-
-
-    // Mesma operação anterior com cache usando arrow function
-    // $post = cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
-
-
-    // Migrando o recebimento do POST para um Models
-
-    return view('post', [
-      'post' => $post
-    ]);
-});
-
-
-Route::get('/categories/{category:slug}', function (Category $category){
-
-
-    return view('posts', [
-        'posts' => $category->posts,
-        'categories' => Category::all(),
-        'currentCategory' => $category
-    ]);
-})->name('category');
-
-
-Route::get('/authors/{author:username}', function (User $author){
-
-
-    return view('posts', [
-        'posts' => $author->posts,
-        'categories' => Category::all()
-    ]);
-});
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
